f06599ab1151c84ca513a794fce86bb74ab703f0
Bartosz Woldanski <studio@dziwnykot.pl>
2022-04-04 19:56:20 +0200

    feat: files, styles, components organization
    Add STYLES folder and move to it all styles files.
    Add CONTROLPANEL component.
    Add draft of HTML view organization in App component with draft
    of useState.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import react, { useState } from "react";
 import "./styles/App.css";
+import ControlPanel from "./ControlPanel";
+import Sample from "./Sample";
 
 function App() {
+  const [colorText, setColorText] = useState('');
+  const [colorBackground, setColorBackground] = useState('');
   
   return (
     <div className="App">
-
+      <div className="samples">
+        <Sample colorBackground={""} colorText={""} />
+        <Sample colorBackground={""} colorText={""} />
+        <Sample colorBackground={""} colorText={""} />
+      </div>
+      <ControlPanel props={ ''}/>
     </div>
   );
 }
---
+++ src/ControlPanel.tsx
@@ -0,0 +1,15 @@
+import react from 'react';
+
+interface ControlPanelProps {
+props: any ;
+}
+
+const ControlPanel = ({ props}: ControlPanelProps) => {
+     
+        return (
+            <>
+                <div className='control-panel'>  </div>
+            </>
+        )
+};
+export default ControlPanel;
